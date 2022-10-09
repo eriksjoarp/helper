d804f3e0b46931308753c780ca534e79f6b664cd
eriksjoarp <erik.westring@gmail.com>
2022-10-09 21:48:13 +0200

    help functions to manipulate images. added functnion to split one large image into a grid of smaller ones. and save them

---
+++ erik_functions_images.py
@@ -0,0 +1,98 @@
+import os
+import cv2
+from helper import erik_functions_files
+
+
+
+
+# same size  and horizontally
+def images_concatenate(list_2d):
+    # return final image
+    return cv2.vconcat([cv2.hconcat(list_h) for list_h in list_2d])
+
+    # image resizing
+    img1_s = cv2.resize(img1, dsize=(0, 0), fx=0.5, fy=0.5)
+
+    # function calling
+    img_tile = concat_vh([[img1_s, img1_s, img1_s],
+                          [img1_s, img1_s, img1_s],
+                          [img1_s, img1_s, img1_s]])
+    # show the output image
+    cv2.imshow('concat_vh.jpg', img_tile)
+
+
+def image_split_save(image_cuts, path_image, dir_output=None):
+    os.makedirs(dir_output, exist_ok=True)
+
+    counter = 1
+
+    # dir, filename_base, file_extension from full path
+    dir_name, file_name, file_ext = erik_functions_files.path_split_from_path(path_image)
+
+    if dir_output == None:
+        dir_output = dir_name
+
+    for image_cut in image_cuts:
+        filename_new = file_name + '_' + str(counter) + file_ext
+        path_save = os.path.join(dir_output, filename_new)
+        cv2.imwrite(path_save, image_cut)
+        counter += 1
+
+
+# get crop from range of h,w with size
+def get_crop(h_start,w_start, h_size,w_size):
+    w_coord_start = (w_start * w_size)
+    h_coord_start = (h_start * h_size)
+    w_coord_end = (w_start+1) * w_size
+    h_coord_end = (h_start+1) * h_size
+    return w_coord_start,w_coord_end, h_coord_start, h_coord_end
+
+
+# splits an image into smaller cuts in a gridlike fashion, it saves the slices if save_path is set to something
+def image_split(path_image, size_split=64, save_path=None):
+    # check if image exists on file
+    if not(erik_functions_files.file_exists(path_image)): return False
+
+    image_cuts = []
+    image = cv2.imread(path_image)
+    (h, w) = image.shape[:2]
+
+    # compute the size for every cut
+    h_cuts = int(h // size_split)
+    w_cuts = int(w // size_split)
+
+    for h_cut in range(h_cuts-1):
+        for w_cut in range(w_cuts-1):
+            w1, w2, h1, h2 = get_crop(h_cut, w_cut, size_split, size_split)
+            image_cut = image[h1:h2,w1:w2]
+            image_cuts.append(image_cut)
+
+    if save_path != None:
+        image_split_save(image_cuts, path_image, save_path)
+    return image_cuts
+
+
+'''
+# splits an image into smaller cuts in a gridlike fashion, it saves the slices if save_path is set to something
+# it crops the center portions
+def image_split_crop(path_image, size_crop, save_path=None):
+    # check if image exists on file
+    if not(erik_functions_files.file_exists(path_image)): return False
+
+    image_cuts = []
+    image = cv2.imread(path_image)
+    (h, w) = image.shape[:2]
+
+    # compute the size for every cut
+    h_numbers = int(h // size_crop)
+    w_new = int(w // grid_size)
+
+    for h_cut in range(grid_size):
+        for w_cut in range(grid_size):
+            image_cut = image[h_new * h_cut : h_new * (h_cut + 1), w_new * w_cut : w_new * (w_cut + 1)]
+            image_cuts.append(image_cut)
+
+    if save_path != None:
+        image_split_save(image_cuts, path_image, save_path)
+    return image_cuts
+'''
