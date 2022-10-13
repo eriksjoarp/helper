53e95e6c76654c2de54250fa7bf3fd525c83570a
eriksjoarp <erik.westring@gmail.com>
2022-10-13 20:52:02 +0200

    bugfixes

--- erik_functions_images.py
+++ erik_functions_images.py
@@ -72,14 +72,15 @@ def image_split(path_image, size_split=64, save_path=None):
         image_split_save(image_cuts, path_image, save_path)
     return image_cuts
 
-<<<<<<< HEAD
+
 # takes an array of images and splits them as a grid and saves the portions in a new directory
 def split_images_into_new_dir(dir_images, dir_images_out, size):
     images = erik_functions_files.files_images_in_dir(dir_images)
     for path_image in images:
         #   def image_split(path_image, grid_size=2, save_path=None):
         image_split(path_image, size, dir_images_out)
-=======
+
+
 def group_images_by_category_into_directorys(labels, label_names, path_images, dir_save):
     np_labels = np.array(labels)
     np_unique_labels = np.unique(np_labels)
@@ -93,7 +94,6 @@ def group_images_by_category_into_directorys(labels, label_names, path_images, d
         filename = os.path.basename(path_images[i])
         path_save_image = os.path.join(dir_save, labels[i], filename)
         shutil.copyfile(path_images[i], path_save_image)
->>>>>>> 7a6e28cb7bc220f6b2e1b91211d42ee5983d4f9d
 
 
 
